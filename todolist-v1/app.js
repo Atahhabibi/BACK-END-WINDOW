24a1af1b5cd15f74c2a45471f820714694e4a9c5
atah <habibiatah@gmail.com>
2023-06-15 06:16:36 -0700

    progrss

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,10 +1,35 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: "true" }));
-var newItems = ["Buy Food", "Cook Food", "Eat food"];
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB", {
+  useNewUrlParser: true,
+});
+
+const itemsSchema = {
+  name: String,
+};
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "Welcome to you todoList!",
+});
+
+const item2 = new Item({
+  name: "Click Add Button to add new Item ",
+});
+
+const item3 = new Item({
+  name: "HIt this to delete the item",
+});
+
+const defaultItems = [item1, item2, item3];
 
 app.listen("3000", (req, res) => {
   console.log("SERVER LISTENING PORT : 3000");
@@ -18,14 +43,37 @@ app.get("/", (req, res) => {
     day: "numeric",
     month: "long",
   };
-
   var day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { kindOfDay: day, newListItems: newItems });
+  Item.find()
+    .then((result) => {
+      if (result.length === 0) {
+        Item.insertMany(defaultItems)
+          .then(() => {
+            console.log("Successfully saved deafultItems to database");
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+        res.redirect("/");
+      }
+
+      const newArray = result.map((item) => item.name);
+      res.render("list", { kindOfDay: day, newListItems: newArray });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.post("/", (req, res) => {
-  item = req.body.item;
-  newItems.push(item);
+  const tempName = req.body.item;
+
+  const item = new Item({
+    name: tempName,
+  });
+
+  item.save();
+
   res.redirect("/");
 });
