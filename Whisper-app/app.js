b248680d6b5807ed02be8a163bac4830ddefce06
atahhabibi <habibiatah@gmail.com>
2023-06-20 13:48:05 -0700

    c

--- Whisper-app/app.js
+++ Whisper-app/app.js
@@ -1,13 +1,11 @@
-
 require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
-
-
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const bcrypt = require("bcrypt");
+const saltRounds = 13;
 
 mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 
@@ -15,13 +13,6 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
-
-const secret="thisisourlittlesecret."
-userSchema.plugin(encrypt, {
-  secret: process.env.SECRET_KEY,
-  encryptedFields: ["password"],
-});
-
 
 const User = new mongoose.model("User", userSchema);
 
@@ -48,29 +39,38 @@ app.post("/register", (req, res) => {
   const username = req.body.username;
   const password = req.body.password;
 
-  const newUser = new User({
-    email: username,
-    password: password,
-  });
+  bcrypt
+    .hash(password, saltRounds)
+    .then((hash) => {
+      const newUser = new User({
+        email: username,
+        password: hash,
+      });
 
-  newUser
-    .save()
-    .then(() => {
-      res.render("secrets");
+      newUser
+        .save()
+        .then(() => {
+          res.render("secrets");
+        })
+        .catch((err) => console.log(err));
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      console.log(err);
+    });
 });
 
 app.post("/login", (req, res) => {
   const email = req.body.username;
   const password = req.body.password;
 
-  User.findOne({ email})
+  User.findOne({ email })
     .then((result) => {
       if (result) {
-        if (result.password === password) {
-          res.render("secrets");
-        }
+        bcrypt.compare(password, result.password).then((exist) => {
+          if (exist) {
+            res.render("secrets");
+          }
+        });
       } else {
         res.render("No user exist with such email and password");
       }
