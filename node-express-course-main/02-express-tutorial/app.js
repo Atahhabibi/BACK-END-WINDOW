1f5e075668ad08a5e96c894bd338aba134444f9f
atah <habibiatah@gmail.com>
2023-06-30 01:16:35 -0700

    Express Basic example

--- node-express-course-main/02-express-tutorial/app.js
+++ node-express-course-main/02-express-tutorial/app.js
@@ -1,55 +1,26 @@
-const http = require("http");
-const { readFileSync } = require("fs");
+const express=require('express');
 
-//get all files
-
-const homePage = readFileSync("./navbar-app/index.html");
-const homeStyle= readFileSync("./navbar-app/styles.css");
-const homeImage = readFileSync("./navbar-app/logo.svg");
-const homeLogic = readFileSync("./navbar-app/browser-app.js");
-
-const server = http.createServer((req, res) => {
-  console.log("=========USER HTI THE SERVER===========");
-
-  console.log(req.method);
-  console.log(req.url);
-
-  if (req.url === "/") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write(homePage);
-  } else if (req.url === "/contact") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>CONTACT PAGE<h1/>");
-  }
-   else if (req.url === "/styles.css") {
-    res.writeHead(200, { "content-type": "text/css" });
-    res.write(homeStyle);
-  } 
-   else if (req.url === "/logo.svg") {
-    res.writeHead(200, { "content-type": "image/svg+xml" });
-    res.write(homeImage);
-  } 
-   else if (req.url === "/browser-app.js") {
-    res.writeHead(200, { "content-type": "text/javascript" });
-    res.write(homeLogic);
-  } 
-   else if (req.url === "/about") {
-    res.writeHead(200, { "content-type": "text/html" });
-    res.write("<h1>ABOUT PAGE<h1/>");
-  } 
+const app=express();
 
 
-  
-  
-  
-  else {
-    res.writeHead(404, { "content-type": "text/html" });
-    res.write("<h1>ERROR PAGE<h1/>");
-  }
+app.listen(3000,()=>{
+  console.log("*************SERVER LISTENING PORT: 3000 ************");
+})
 
-  res.end();
-});
 
-server.listen(3000, (req, res) => {
-  console.log("*********SERVER CONNECTED 3000 *********");
-});
+app.get('/',(req,res)=>{
+  res.status(200).send('<h1>Home Page</h1>')
+})
+
+app.get('/about',(req,res)=>{
+  res.status(200).send('<h1>About Page</h1>')
+})
+
+
+app.get('/about',(req,res)=>{
+  res.status(404).send('<h1>About Page</h1>')
+})
+
+app.all('*',(req,res)=>{
+  res.send('<h1>PAGE NOT FOUND</h1>')
+})
