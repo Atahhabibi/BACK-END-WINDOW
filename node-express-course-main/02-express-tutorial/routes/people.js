b7d66a8ef6f5c7a93a7700ff0192f82b95b4d508
atah <habibiatah@gmail.com>
2023-07-01 07:11:34 -0700

    create express.Router() in difrerent file

--- node-express-course-main/02-express-tutorial/routes/people.js
+++ node-express-course-main/02-express-tutorial/routes/people.js
@@ -6,19 +6,23 @@ const {
   createPersonPostman,
   deletePerson,
   upDatePerson,
-  getPeople
+  getPeople,
 } = require("../Controllers/people");
 
 const router = express.Router();
 
-router.get("/", getPeople);
+// router.get("/", getPeople);
 
-router.post("/postman",createPersonPostman);
+// router.post("/postman",createPersonPostman);
 
-router.put("/:id",upDatePerson);
+// router.put("/:id",upDatePerson);
 
-router.post("/",createPerson);
+// router.post("/",createPerson);
 
-router.delete("/:id",deletePerson);
+// router.delete("/:id",deletePerson);
+
+router.route("/").get(getPeople).post(createPerson);
+router.route("/postman").post(createPersonPostman);
+router.route("/:id").delete(deletePerson).put(upDatePerson);
 
 module.exports = router;
