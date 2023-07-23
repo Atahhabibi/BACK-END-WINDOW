a410d9b860947cc54fde8c8390274572aa8020bd
atah <habibiatah@gmail.com>
2023-07-23 07:29:07 -0700

    s

--- JOBS-API/app.js
+++ JOBS-API/app.js
@@ -1,9 +1,23 @@
 require("dotenv").config();
 require("express-async-errors");
+
+const helmet=require('helmet');
+const cors=require('cors');
+const xss=require('xss-clean')
+const rateLimiter=require('express-rate-limit')
+
 const express = require("express");
 const app = express();
 const connectDB = require("./db/connect");
+app.set('trust proxy',1);
+app.use(rateLimiter({
+  windowMs:15*60*1000,
+  max:100,
+}))
 app.use(express.json());
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 const authenticateUser=require('./middleware/authentication')
 
 const authRouter = require("./routes/auth");
@@ -11,6 +25,7 @@ const jobsRouter = require("./routes/jobs");
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs",authenticateUser, jobsRouter);
+
 
 // error handler
 const notFoundMiddleware = require("./middleware/not-found");
